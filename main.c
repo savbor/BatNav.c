12cc4dfc7ac4d9910382ad9e618183ae2b51b7f5
Riccardo <riccardoideaemail@gmail.com>
2016-12-20 14:35:59 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,20 @@
+/* Programma per Giocare a BattagliaNavale */
+
+#include<stdio.h>
+#include<stdlib.h>
+#include"intest.h" 	/* Header file si inseriscono con le "" */
+
+
+int main(){
+	/* Dichiarazione Variabili */	
+	//int i;
+
+
+	/* Svolgimento */
+   	intro();
+
+	/* Conclusione */
+	return 0;	
+	
+}
+
